ddb7cc6ce352c844f6e66c8f2a66c2acb5a781db
Nallamilliveerraghavareddy <nveerraghavareddy@gmail.com>
2018-02-01 11:22:08 +0000

    modified:   README.md 	modified:   app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright 2017 Prasanna Venkadesh
-#
-# This file is part of osm-leaderboard
-#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
